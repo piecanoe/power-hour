127b8aeb71a6422d86fbe717e370dda74c39585d
Captain Du <karenkaydu@gmail.com>
2024-01-17 15:29:07 -0800

    Add yelp api integration

---
+++ controllers/yelp_api.js
@@ -0,0 +1,41 @@
+const axios = require('axios')
+
+
+module.exports = {
+    searchBusiness: async function(term, location) {
+      try {
+        const response = await axios.get('https://api.yelp.com/v3/businesses/search', {
+          headers: {
+            Authorization: `Bearer ${process.env.TOKEN}`
+          },
+          params: {
+            term, location
+          }
+        });
+        return response.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  
+    searchByPhone: async function(phone) {
+      try {
+        const response = await axios.get('https://api.yelp.com/v3/businesses/search/phone', {
+          headers: {
+            Authorization: `Bearer ${process.env.TOKEN}`
+          },
+          params: {
+            phone
+          }
+        });
+        return response.data;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+  
+  (async () => {
+    const businesses = await module.exports.searchBusiness('food', 'Newyork');
+    console.log(businesses);
+  })();
